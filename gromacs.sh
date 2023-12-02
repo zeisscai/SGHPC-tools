61ae80b6eafd74157d200d43ea055d07811feae2
zeiss <zeiss.cai@outlook.com>
2023-12-02 16:18:37 +0800

    mas

--- gromacs.sh
+++ gromacs.sh
@@ -36,3 +36,34 @@ else
   echo "This script only supports Rocky Linux. Exiting..."
   exit 1
 fi
+
+# 检查是否存在gromacs开头的tar.gz压缩包
+if ls gromacs*.tar.gz 1> /dev/null 2>&1; then
+  # 解压缩压缩包
+  tar xfz gromacs*.tar.gz
+  cd gromacs-*/ || exit
+
+  # 创建build目录并进入
+  mkdir build
+  cd build || exit
+
+  # 使用cmake进行配置
+  cmake .. -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON
+
+  # 编译
+  make
+
+  # 运行测试
+  make check
+
+  # 安装
+  sudo make install
+
+  # 设置环境变量
+  source /usr/local/gromacs/bin/GMXRC
+
+  # 打印安装输出
+  echo "Gromacs已成功安装"
+else
+  echo "未找到gromacs开头的tar.gz压缩包"
+fi
